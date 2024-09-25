b7938f7561e884376a73b686580fbee8c7f61cca
DrTonks <3064517736@qq.com>
2024-09-25 17:47:33 +0800

    1

--- js/index.js
+++ js/index.js
@@ -5,3 +5,16 @@
         console.log('Searching for:', query);
     })
     
+    function getnav(){
+        let sidebar =document.getElementById("sidebar");
+        let overlay =document.getElementById("overlay");
+        if (sidebar.style.width === "250px"){
+            sidebar.style.width === "0";
+            overlay.style.display === "none";
+        }
+        else{
+            sidebar.style.width === "250px";
+            overlay.style.display === "block";
+        }
+
+    }
