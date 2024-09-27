a481e282282723c2a04b05224454e203bc5d6d4c
DrTonks <3064517736@qq.com>
2024-09-27 09:45:34 +0800

    手机测试

---
+++ js/blog.js
@@ -0,0 +1,12 @@
+function title(){
+    let title=document.getElementById('title')
+    if (window.innerWidth<= 768){
+        title.innerText='NEW！'
+    }
+    else{
+        title.innerText='最近文章'
+        
+    }
+}//尝试用js搞定移动端的适配，果然比css隐藏/显示要简单...
+window.onload = title;
+window.onresize = title;
