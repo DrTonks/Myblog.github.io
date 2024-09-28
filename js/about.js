789956d611a2c72147ab5b09a83bc9e0cd231dfe
DrTonks <3064517736@qq.com>
2024-09-28 19:21:24 +0800

    最终版1

--- js/about.js
+++ js/about.js
@@ -1,16 +1,42 @@
-function boxtext(){
-    let boxtext=document.getElementsByClassName('boxtext')
+// function boxtext(){
+//     let boxtext=document.getElementsByClassName('boxtext')
 
-    if (window.innerWidth<= 768){
-        boxtext[0].innerText=''
-        boxtext[1].innerText='NEW!'
-        boxtext[2].innerText='NEW!'
-        boxtext[3].innerText='NEW!'
+//     if (window.innerWidth<= 768){
+//         boxtext[0].innerText=''
+//         boxtext[1].innerText='NEW!'
+//         boxtext[2].innerText='NEW!'
+//         boxtext[3].innerText='NEW!'
+//     }
+//     else{
+        
+//     }
+// }
+// //因为工程量不大，而且人笨，所以用这种办法，空间换时间，轻喷qwq
+// window.onload = boxtext;
+// window.onresize = boxtext;
+let initText = []; // 用于保存初始文本
+
+function boxtext() {
+    let boxtext = document.getElementsByClassName('boxtext');
+
+ //做一个保存文本，否则浏览器窗口变动时手机端文本会被保存在电脑端
+    if (initText.length === 0) {
+        for (let i = 0; i < boxtext.length; i++) {
+            initText[i] = boxtext[i].innerText;
+        }
     }
-    else{
-     
+    //手机端文本
+    if (window.innerWidth <= 768) {
+        boxtext[0].innerText = '是一名来自福建的大一新生。\n网名的来源是小学期间同学们用哈利波特里面的名字来相互称呼对方，并且沿用至今。\n（由于毕业卡片上面同学给我错写的英文名，后来一度把Tanks当作尼法朵拉的姓氏qwq）';
+        boxtext[1].innerText = '喜欢写作。偶尔有点子了就去作家助手上面写点玩意儿放松。童年梦想是成为一名作家，现在也是。\n喜欢和同学们在一起玩，但是不喜欢出门，也不喜欢参与太多社交活动（比如班上团建）';
+        boxtext[2].innerText = '想当高手。\n想当机器学习高手。\n想当前端开发高手。\n想当计算机C语言高手。\n唉唉，想得太多，做得太少。';
+        boxtext[3].innerText = '想要做出一些属于自己的东西，这是我选择前端的原因。想要让AI变得越来越像人，这是我尝试机器学习的原因。\n我不会停止思考，不会停止创造，不会因为未来的不确定而畏葸不前。\nAI可以越来越像人，但人不能越来越像AI。';
+    } else {
+        for (let i=0; i<boxtext.length; i++) {
+            boxtext[i].innerText = initText[i]; 
     }
-}
-//因为工程量不大，而且人笨，所以用这种办法，空间换时间，轻喷qwq
-window.onload = boxtext;
-window.onresize = boxtext;
+}}
+
+
+window.addEventListener('resize', boxtext);
+boxtext();
